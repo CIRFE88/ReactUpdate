52cd51d21bcfd90c86e4f224384d29816862eb50
Ibraahim Cirfe <ibraahimcirfe@gmail.com>
2023-05-23 14:55:06 +0300

    first commit

---
+++ assignmentiupdate/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UpdateStates from "./UpdateStates";
+
+
+
+function App() {
+  return (
+    <div>
+      <UpdateStates />
+    </div>
+  );
+}
+
+export default App;
